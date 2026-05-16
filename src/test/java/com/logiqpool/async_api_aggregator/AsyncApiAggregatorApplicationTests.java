b4be57abab61056d8ce93f6bc1e27dd0bee033cd
Namrata Chikhle <namrata.chikhle@gmail.com>
2026-05-17 00:06:34 +0100

    chore: initialize async-api-aggregator project

---
+++ src/test/java/com/logiqpool/async_api_aggregator/AsyncApiAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logiqpool.async_api_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApiAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
