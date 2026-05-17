561c0a79f151229379d5db0362023cf8d3a65555
Namrata Chikhle <namrata.chikhle@gmail.com>
2026-05-17 15:45:38 +0100

    feat: add DashboardController Get(api/dashboard)

--- src/main/java/com/logiqpool/async_api_aggregator/controller/DashboardController.java
+++ src/main/java/com/logiqpool/async_api_aggregator/controller/DashboardController.java
@@ -1,4 +1,23 @@
 package com.logiqpool.async_api_aggregator.controller;
 
+import com.logiqpool.async_api_aggregator.dto.DashboardResponse;
+import com.logiqpool.async_api_aggregator.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dashboard")
+@RequiredArgsConstructor
 public class DashboardController {
+    public final DashboardService dashboardService;
+
+    @GetMapping
+    public ResponseEntity<DashboardResponse> getDashboard(){
+        DashboardResponse dashboardResponse =dashboardService.getDashboardResponse();
+        return ResponseEntity.status(HttpStatus.OK).body(dashboardResponse);
+    }
 }
